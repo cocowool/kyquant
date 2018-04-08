10d481502bd52639dcb83e636ad9fc141a3a0a29
Green Wang <cocowool@qq.com>
2018-04-08 17:39:23 +0800

    pandas basic

---
+++ python/pandas_basic.py
@@ -0,0 +1,46 @@
+# Pandas 基础
+
+import pandas as pd
+import numpy as np
+
+stock_day_change = np.load('./stock_day_change.npy')
+stock_day_change.shape
+
+pd.DataFrame(stock_day_change).head()
+pd.DataFrame(stock_day_change).head(10)
+pd.DataFrame(stock_day_change)[:5]
+
+stock_symbols = ['股票' + str(x) for x in range(stock_day_change.shape[0])]
+
+pd.DataFrame(stock_day_change, index=stock_symbols).head(2)
+
+days = pd.date_range('2017-1-1', periods=stock_day_change.shape[1], freq='1d')
+
+df = pd.DataFrame(stock_day_change, index=stock_symbols, columns=days)
+df.head(2)
+
+# 进行数据转置
+df = df .T
+df.head()
+
+# 这个用法不推荐了
+df_20 = df.resample('21D',how='mean')
+df_20.head()
+
+df_20 = df.resample('21D').mean()
+df_20.head()
+
+df_stock0 = df['股票0']
+print(type(df_stock0))
+df_stock0.head()
+
+df_stock0.cumsum().plot()
+
+
+# 使用 ohlc 方法重采样 open, high, low, close
+# 周线
+df_stock0_5 = df_stock0.cumsum().resample('5D').ohlc()
+# 月线
+df_stock0_20 = df_stock0.cumsum().resample('21D').ohlc()
+df_stock0_5.head()
+
