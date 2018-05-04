2569bfbb46eb5e89fa3d4f5130cc4ca61bd73572
Green Wang <cocowool@qq.com>
2018-05-04 14:03:36 +0800

    candle plot / bokeh

--- python/matplot.py
+++ python/matplot.py
@@ -44,4 +44,23 @@ drawer = axs[1][1]
 plot_demo(drawer)
 drawer.legend(['Series', 'Numpy', 'List'], bbox_to_anchor=(1.05,1), loc=2, borderaxespad=0.)
 
+# 绘制K线图，通过 matplotlib.finance.candlestick_ochl 以及 matplotlib.finance.candlestick2_ohlc 函数
+import matplotlib.finance as mpf
+__colorup__ = "red"
+__colordown__ = "green"
+tsla_part_df = tsla_df[:30]
 
+fig, ax = plt.subplots(figsize=(14,7))
+quotes = []
+for index, (d,o,c,h,l) in enumerate(
+	zip(tsla_part_df.index, tsla_part_df.open, tsla_part_df.close, tsla_part_df.high, tsla_part_df.low)):
+	d = mpf.date2num(d)
+	val = (d,o,c,h,l)
+	quotes.append(val)
+mpf.candlestick_ochl(ax,quotes,width=0.6,colorup=__colorup__,colordown=__colordown__)
+ax.autoscale_view()
+ax.xaxis_date()
+
+# 引用 Bokeh 支持交互可视化
+from abupy import ABuMarketDrawing
+ABuMarketDrawing.plot_candle_form_klpd(tsla_df,html_bk=True)
