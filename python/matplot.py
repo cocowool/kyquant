5cba8e0144b74ca0755541bddcddb574ecb546c6
Green Wang <cocowool@qq.com>
2018-05-31 15:51:42 +0800

    modify kyquant

--- python/matplot.py
+++ python/matplot.py
@@ -91,8 +91,141 @@ pd.rolling_mean(tsla_df.close, window=90).plot()
 plt.legend(['close', '30 mv', '60 mv', '90 mv'], loc='best')
 
 # 验证低开高走第二天趋势
+low_to_high_df = tsla_df.iloc[tsla_df[(tsla_df.close > tsla_df.open) & ( tsla_df.key != tsla_df.shape[0] -1)].key.values + 1]
+change_ceil_floor = np.where(low_to_high_df['p_change'] > 0, np.ceil(low_to_high_df['p_change']), np.floor(low_to_high_df['p_change']))
+change_ceil_floor = pd.Series(change_ceil_floor)
+print('低开高收下一个交易日所有下跌的跌幅取整数和 sum:' + str(change_ceil_floor[change_ceil_floor < 0].sum()))
+print("低开高收下一个交易日所有上涨的涨幅取整数和 sum:" + str(change_ceil_floor[change_ceil_floor > 0].sum()))
+# 2*2 4张子图
+_, axs = plt.subplots(nrows=2, ncols=2, figsize=(12,10))
+# 柱图
+change_ceil_floor.value_counts().plot(kind='bar', ax=axs[0][0])
+# 水平柱状图
+change_ceil_floor.value_counts().plot(kind='barh', ax=axs[0][1])
+# 概率密度图
+change_ceil_floor.value_counts().plot(kind='kde', ax=axs[1][0])
+# 饼图
+change_ceil_floor.value_counts().plot(kind='pie', ax=axs[1][1])
 
 
 # searborn
 import seaborn as sns
 sns.distplot(tsla_df['p_change'], bins=80)
+
+sns.boxplot(x='date_week', y='p_change', data=tsla_df)
+
+# 可视化两组数据的相关性及概率密度分布
+sns.jointplot(tsla_df['high'], tsla_df['low'])
+
+change_df = pd.DataFrame({'tsla':tsla_df.p_change})
+# join google
+change_df = change_df.join(pd.DataFrame({'goog':ABuSymbolPd.make_kl_df('usGOOG', n_folds=2).p_change}), how='outer')
+# join apple
+change_df = change_df.join(pd.DataFrame({'aapl':ABuSymbolPd.make_kl_df('usAAPL', n_folds=2).p_change}), how='outer')
+# join facebook
+change_df = change_df.join(pd.DataFrame({'fb':ABuSymbolPd.make_kl_df('usFB', n_folds=2).p_change}), how='outer')
+# join baidu
+change_df = change_df.join(pd.DataFrame({'bidu':ABuSymbolPd.make_kl_df('usBIDU', n_folds=2).p_change}), how='outer')
+change_df = change_df.dropna()
+change_df.head()
+
+corr = change_df.corr()
+_, ax = plt.subplots(figsize(8,5))
+sns.heatmap(corr,ax=ax)
+
+
+# CASE 1 可视化量化策略的交易区间
+def plot_trade(buy_date, sell_date):
+	start = tsla_df[tsla_df.index == buy_date].key.values[0]
+	end = tsla_df[tsla_df.index == sell_date].key.values[0]
+
+	plot_demo(just_series=True)
+	plt.fill_between(tsla_df.index, 0, tsla_df['close'], color='blue', alpha=.08)
+
+	# 标注股票持有周期为绿色，使用 start end 切片周期
+	if tsla_df['close'][end] < tsla_df['close'][start]:
+		plt.fill_between(tsla_df.index[start:end], 0, tsla_df['close'][start:end], color='green', alpha=.38)
+	else:
+		plt.fill_between(tsla_df.index[start:end], 0, tsla_df['close'][start:end], color='red', alpha=.38)
+
+	plt.ylim(np.min(tsla_df['close']) -5, np.max(tsla_df['close']) + 5 )
+
+	plt.legend(['close'], loc='best')
+
+# 注意日期不能是周末
+plot_trade('2018-01-29', '2018-04-16')
+
+# CASE 2 标注卖出的原因
+def plot_trade_with_annotate(buy_date, sell_date, annotate):
+	plot_trade(buy_date, sell_date)
+	plt.annotate(annotate, xy=(sell_date, tsla_df['close'].asof(sell_date)), arrowprops=dict(facecolor='yellow'), horizontalalignment='left', verticalalignment='top')
+
+plot_trade_with_annotate('2018-01-29', '2018-04-16', '止损卖出')
+
+
+# Example 2 标准化两个股票的观察周期
+goog_df = ABuSymbolPd.make_kl_df('usGOOG', n_folds=2)
+print(round(goog_df.close.mean(), 2))
+print(round(goog_df.close.median(), 2))
+goog_df.tail()
+
+def plot_two_stock(tsla, goog, axs=None):
+	drawer = plt if axs is None else axs
+	drawer.plot(tsla, c='r')
+	drawer.plot(goog, c='g')
+
+	drawer.grid(True)
+	drawer.legend(['tsla', 'google'], loc='best')
+
+plot_two_stock(tsla_df.close, goog_df.close)
+plt.title('TSLA and Google Close')
+plt.xlabel('time')
+plt.ylabel('close')
+
+# 四种数据标准化处理的方式
+def two_mean_list(one, two, type_look='look_max'):
+	one_mean = one.mean()
+	two_mean = two.mean()
+
+	if type_look == 'look_max':
+		one, two = (one, one_mean/two_mean * two) if one_mean > two_mean else ( one * two_mean/one_mean, two)
+	elif type_look == 'look_min':
+		one, two = (one * two_mean/one_mean ,two) if one_mean > two_mean else ( one, two * one_mean/two_mean)
+
+def regular_std(group):
+	return (group - group.mean()) / group.std()
+
+def regular_mm(group):
+	return (group - group.min())/(group.max() - group.min())
+
+_, axs = plt.subplots(nrows=2, ncols=2, figsize=(14,10))
+drawer = axs[0][0]
+plot_two_stock(regular_std(tsla_df.close), regular_std(goog_df.close), drawer)
+drawer.set_title('(group-group.mean())/group.std()')
+
+drawer = axs[0][1]
+plot_two_stock(regular_mm(tsla_df.close), regular_mm(goog_df.close), drawer)
+drawer.set_title('(group-group.min())/(group.max() - group.min())')
+
+drawer = axs[1][0]
+one, two = two_mean_list(tsla_df.close, goog_df.close, type_look='look_max')
+plot_two_stock(one, two, drawer)
+drawer.set_title('two_mean_list type_look=look_max')
+
+drawer = axs[1][1]
+one, two = two_mean_list(tsla_df.close, goog_df.close, type_look='look_min')
+plot_two_stock(one, two, drawer)
+drawer.set_title('two_mean_list type_look=look_min')
+
+
+
+
+
+
+
+
+
+
+
+
+
